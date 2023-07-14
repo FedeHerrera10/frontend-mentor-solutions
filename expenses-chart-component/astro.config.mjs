a1a425316924495741f2ddf1d546344b7e18d57a
Fede Herrera <herrera3299@gmail.com>
2023-07-14 19:04:07 -0300

    deploy astro

--- expenses-chart-component/astro.config.mjs
+++ expenses-chart-component/astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://fedeherrera10.github.io/frontend-mentor-solutions/',
-  base: '/expenses-char-component',
+  site: 'https://fedeherrera10.github.io/frontend-mentor-solutions/'
 });
