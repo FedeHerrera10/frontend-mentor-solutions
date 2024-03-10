fd9b6abf6747a2bd7c1268f41c187277fa75f9d5
Fede Herrera <herrera3299@gmail.com>
2024-03-10 15:03:53 -0300

    update config astro

--- interactive-card-detail-form/astro.config.mjs
+++ interactive-card-detail-form/astro.config.mjs
@@ -5,10 +5,5 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site:'https://fedeherrera10.github.io',
-  base:'/frontend-mentor-solutions',
-  integrations: [tailwind(), react()]
-},
-
-
-);
+ integrations: [tailwind(), react()]
+});
